396775b47f9311d9dc49f85492b1f119c70f3299
Attila M. Magyar <attila.m.magyar@gmail.com>
2026-02-04 00:49:08 +0100

    Win32Platform: make the constructor explicit (fix Cppcheck warning)

--- src/gui/win32.hpp
+++ src/gui/win32.hpp
@@ -45,7 +45,7 @@ namespace MpeEmulator
 class Win32Platform
 {
     public:
-        Win32Platform(HINSTANCE dll_instance);
+        explicit Win32Platform(HINSTANCE dll_instance);
         ~Win32Platform();
 
         HINSTANCE get_dll_instance() const;
