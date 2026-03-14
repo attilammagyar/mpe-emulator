15b64ea997822fa2246d090f83458dacf27ca75d
Attila M. Magyar <attila.m.magyar@gmail.com>
2026-03-15 00:34:24 +0100

    make: wrap `leaks` in a custom script so that the exit code of the tests can be propagated
    The exit code of `leaks` on macOS only tells whether there was a memory
    leak, but it does not tell whether the process exited successfully.

---
+++ scripts/mac_check_leaks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+###############################################################################
+# This file is part of MPE Emulator
+# Copyright (C) 2026  Attila M. Magyar
+#
+# MPE Emulator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MPE Emulator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
+
+set -e
+set -u
+set -o pipefail
+
+main()
+{
+    $@ && leaks -quiet -atExit -- $@
+}
+
+main "$@"
